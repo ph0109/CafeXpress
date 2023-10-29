e08583ce324b9802069ea8a33a728fa97e12e823
Woohee Kim <rladngml2002@naver.com>
2023-10-29 20:47:32 +0900

    second commit

--- app/src/main/java/com/projectdev/cafexpress/Menu_take_Activity.java
+++ app/src/main/java/com/projectdev/cafexpress/Menu_take_Activity.java
@@ -2,13 +2,37 @@ package com.projectdev.cafexpress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 public class Menu_take_Activity extends AppCompatActivity {
-
+    LinearLayout americano;
+    LinearLayout latte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_take);
+
+        americano = (LinearLayout) findViewById(R.id.americano);
+        latte = (LinearLayout)findViewById(R.id.latte);
+        americano.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_take_Activity.this, OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        latte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_take_Activity.this, OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
--- app/src/main/java/com/projectdev/cafexpress/Menu_visit_Activity.java
+++ app/src/main/java/com/projectdev/cafexpress/Menu_visit_Activity.java
@@ -2,13 +2,34 @@ package com.projectdev.cafexpress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class Menu_visit_Activity extends AppCompatActivity {
-
+    LinearLayout americano;
+    LinearLayout latte;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_visit);
+
+        americano = (LinearLayout) findViewById(R.id.americano);
+        latte = (LinearLayout)findViewById(R.id.latte);
+        americano.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this, OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        latte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this, OptionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
