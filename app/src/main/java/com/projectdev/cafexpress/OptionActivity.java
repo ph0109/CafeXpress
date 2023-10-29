4d31f5576b4a29c35265b1666b6db77a43a8718b
Woohee Kim <rladngml2002@naver.com>
2023-10-29 23:15:19 +0900

    5 commit

--- app/src/main/java/com/projectdev/cafexpress/OptionActivity.java
+++ app/src/main/java/com/projectdev/cafexpress/OptionActivity.java
@@ -5,11 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class OptionActivity extends AppCompatActivity {
     ImageView optionMenuImg;
     ImageView img;
+    Button optionDel;
+    Button optionCart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,5 +26,15 @@ public class OptionActivity extends AppCompatActivity {
         // XML 에 있는 ImageView 위젯에 이미지 셋팅
         optionMenuImg = (ImageView) findViewById(R.id.optionMenuImg);
         optionMenuImg.setImageDrawable(drawable);
+        optionDel = findViewById(R.id.optionDel);
+        optionCart = findViewById(R.id.optionCart);
+
+        optionDel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+           
+        });
     }
 }
