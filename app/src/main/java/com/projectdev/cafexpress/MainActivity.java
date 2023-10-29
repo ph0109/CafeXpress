a88a734c76a8dc0da44a4a7b004fe4c1969f7167
Woohee Kim <rladngml2002@naver.com>
2023-10-29 16:57:46 +0900

    first commit

--- app/src/main/java/com/projectdev/cafexpress/MainActivity.java
+++ app/src/main/java/com/projectdev/cafexpress/MainActivity.java
@@ -2,13 +2,30 @@ package com.projectdev.cafexpress;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+     Button button_takeaway;
+     Button button_visit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button_takeaway = findViewById(R.id.button_takeaway);
+        button_visit = findViewById(R.id.button_visit);
+
+        button_takeaway.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
