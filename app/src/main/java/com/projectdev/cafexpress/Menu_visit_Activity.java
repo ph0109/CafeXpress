df6217c88130b6a4565fb513a1aca085d2943f88
Woohee Kim <rladngml2002@naver.com>
2023-10-29 21:35:37 +0900

    3 commit

--- app/src/main/java/com/projectdev/cafexpress/Menu_visit_Activity.java
+++ app/src/main/java/com/projectdev/cafexpress/Menu_visit_Activity.java
@@ -8,8 +8,7 @@ import android.view.View;
 import android.widget.LinearLayout;
 
 public class Menu_visit_Activity extends AppCompatActivity {
-    LinearLayout americano;
-    LinearLayout latte;
+    LinearLayout americano,latte,vanillalatte,lemonade,peachtea,strawberrylatte,chocolatte,milkshake,hibiscus;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +16,13 @@ public class Menu_visit_Activity extends AppCompatActivity {
 
         americano = (LinearLayout) findViewById(R.id.americano);
         latte = (LinearLayout)findViewById(R.id.latte);
+        vanillalatte = (LinearLayout)findViewById(R.id.vanillalatte);
+        lemonade = (LinearLayout)findViewById(R.id.lemonade);
+        peachtea = (LinearLayout)findViewById(R.id.peachtea);
+        strawberrylatte = (LinearLayout)findViewById(R.id.strawberrylatte);
+        chocolatte = (LinearLayout)findViewById(R.id.chocolatte);
+        milkshake = (LinearLayout)findViewById(R.id.milkshake);
+        hibiscus = (LinearLayout)findViewById(R.id.hibiscus);
         americano.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -28,6 +34,55 @@ public class Menu_visit_Activity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(Menu_visit_Activity.this, OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        vanillalatte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this,OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        lemonade.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this,OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        peachtea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this,OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        strawberrylatte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this,OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        chocolatte.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this,OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        milkshake.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this,OptionActivity.class);
+                startActivity(intent);
+            }
+        });
+        hibiscus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Menu_visit_Activity.this,OptionActivity.class);
                 startActivity(intent);
             }
         });
